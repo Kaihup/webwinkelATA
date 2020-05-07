a6c8f2572ca7a86fcc5dbdbf2916469d81d0db3d
Kaihup <kai_91@live.nl>
2020-05-07 13:59:12 +0200

    Eerste commit properties eruit

---
+++ src/main/java/nl/atasamen/webwinkel/WebwinkelApplication.java
@@ -0,0 +1,13 @@
+package nl.atasamen.webwinkel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebwinkelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebwinkelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/atasamen/webwinkel/WebwinkelApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.atasamen.webwinkel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebwinkelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
